52020f9204d41b36bc8725d3171496ebc6f9b41c
root <root@ip-10-42-189-21.ec2.internal>
2025-11-29 07:42:43 +0000

    Task 0 done

--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-add = __import__("0-add").add
+add_0 = __import__('add_0').add
 
 a = 1
 b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add_0(a, b)))
