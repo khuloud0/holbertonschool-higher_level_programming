88810dd047911062b32efc1ac490c389c8d99737
root <root@ip-10-42-41-2.ec2.internal>
2025-12-03 11:56:59 +0000

    Fix NaN and infinity handling in add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,26 +3,26 @@
 This module provides a function that adds two integers.
 """
 
-
 def add_integer(a, b=98):
-    """
-    Adds two integers or floats (after casting to int).
-
-    Args:
-        a: first number
-        b: second number, default = 98
-
-    Returns:
-        int: addition of a and b
+    """Add two integers or floats casted to integers.
 
     Raises:
-        TypeError: if a or b is not an integer or float
+        TypeError: if a or b are not integers or floats
+        TypeError: if a or b are NaN or Infinity
     """
-
+    # Check if a is int/float
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
+    # Check if b is int/float
     if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # Reject NaN and Infinity
+    if a != a or a in [float("inf"), float("-inf")]:
+        raise TypeError("a must be an integer")
+
+    if b != b or b in [float("inf"), float("-inf")]:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
