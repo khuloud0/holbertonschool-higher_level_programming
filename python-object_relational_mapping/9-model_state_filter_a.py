073aeffee4e5afe4c2a7645f0b8cfaed2ba03dc0
root <root@ip-10-42-193-88.ec2.internal>
2026-01-14 13:04:20 +0000

    9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Lists all State objects that contain the letter a
-from the database hbtn_0e_6_usa."""
-
+"""
+Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
+"""
 
 import sys
 from sqlalchemy import create_engine
@@ -9,20 +10,28 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     engine = create_engine(
-        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/'
-        f'{database_name}'
-        )
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, database
+        ),
+        pool_pre_ping=True
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%'))\
-        .order_by(State.id).all()
+
+    states = (
+        session.query(State)
+        .filter(State.name.ilike("%a%"))
+        .order_by(State.id)
+        .all()
+    )
 
     for state in states:
-        print(f"{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
