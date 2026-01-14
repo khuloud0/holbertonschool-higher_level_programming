0b5b7cb8cc6941b6274aeea08c9497b8fd4111b5
root <root@ip-10-42-193-88.ec2.internal>
2026-01-14 13:08:05 +0000

    10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
-"""Prints the State object with the name passed as argument
-from the database hbtn_0e_6_usa."""
+"""
+Prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
+"""
 
 import sys
 from sqlalchemy import create_engine
@@ -9,19 +10,16 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     state_name = sys.argv[4]
 
     engine = create_engine(
-        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/'
-        f'{database_name}'
-        )
-    Base.metadata.bind = engine
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    )
 
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
     state = session.query(State).filter(State.name == state_name).first()
