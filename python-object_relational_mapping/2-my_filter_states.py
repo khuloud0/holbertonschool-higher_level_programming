d3809481345d3ee024e4bbcd841f825e43271a24
root <root@ip-10-42-193-88.ec2.internal>
2026-01-14 12:32:10 +0000

    2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Safely displays states where name matches the argument"""
+"""Displays states where name matches the argument (unsafe)"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials and the searched state name
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to the database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -20,16 +18,18 @@ if __name__ == "__main__":
         db=db_name
     )
 
-    # Create a cursor and safely execute a parameterized query
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = \
-     %s ORDER BY id ASC", (state_name,))
 
-    # Fetch and print matching results
-    rows = cur.fetchall()
-    for row in rows:
+    query = (
+        "SELECT id, name FROM states "
+        "WHERE name = '{}' "
+        "ORDER BY states.id ASC;"
+    ).format(state_name)
+
+    cur.execute(query)
+
+    for row in cur.fetchall():
         print(row)
 
-    # Clean up
     cur.close()
     db.close()
