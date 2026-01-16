ccdf43397c66d2caf1be94d91eccc1f8ccfb83a0
khuloud0 <khuloodalgarni@gmail.com>
2026-01-16 20:36:11 +0300

    Update 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, database
+        ),
         pool_pre_ping=True
     )
-
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
